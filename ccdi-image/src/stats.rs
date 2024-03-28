3ea31199875f5499636d5506fcf2d9b4e49ef471
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2024-03-28 13:06:33 -0400

    Changed combine_stats to macro

--- ccdi-image/src/stats.rs
+++ ccdi-image/src/stats.rs
@@ -1,4 +1,4 @@
-use std::cmp::{min, max};
+use std::cmp::{max, min};
 
 use ccdi_common::RgbImage;
 use nanocv::Img;
@@ -26,9 +26,27 @@ pub struct Histogram {
     pub max: u16,
 }
 
+macro_rules! combine_stats {
+    ($($x: expr),*) => {
+        {let mut total = ChannelStats {
+            min: u16::MAX,
+            max: u16::MIN,
+        };
+        $(
+            let stat = $x.stats();
+            total.min = min(total.min, stat.min);
+            total.max = max(total.max, stat.max);
+        )*
+        total}
+    };
+}
+
 impl Histogram {
     pub fn stats(&self) -> ChannelStats {
-        ChannelStats { min: self.min, max: self.max }
+        ChannelStats {
+            min: self.min,
+            max: self.max,
+        }
     }
 
     pub fn max_count(&self) -> usize {
@@ -40,13 +58,13 @@ pub fn compute_image_stats(image: &RgbImage<u16>, size: usize) -> ImageStats {
     let r = compute_histogram(image.red(), size, compute_channel_stats(image.red()));
     let g = compute_histogram(image.green(), size, compute_channel_stats(image.green()));
     let b = compute_histogram(image.blue(), size, compute_channel_stats(image.blue()));
-    let total = combine_stats(&r.stats(), &combine_stats(&b.stats(), &g.stats()));
+    let total = combine_stats!(r, g, b);
     ImageStats { r, g, b, total }
 }
 
 // =========================================== PRIVATE =============================================
 
-fn compute_channel_stats(channel: &dyn Img<u16>,) -> ChannelStats {
+fn compute_channel_stats(channel: &dyn Img<u16>) -> ChannelStats {
     let mut min_value = u16::MAX;
     let mut max_value = u16::MIN;
 
@@ -57,31 +75,28 @@ fn compute_channel_stats(channel: &dyn Img<u16>,) -> ChannelStats {
         }
     }
 
-    ChannelStats { min: min_value, max: max_value }
+    ChannelStats {
+        min: min_value,
+        max: max_value,
+    }
 }
 
-fn compute_histogram(
-    channel: &dyn Img<u16>,
-    size: usize,
-    stats: ChannelStats
-) -> Histogram {
+fn compute_histogram(channel: &dyn Img<u16>, size: usize, stats: ChannelStats) -> Histogram {
     let mut bins = vec![0; size];
     let divisor = max(1, stats.max - stats.min) as usize;
 
     for line in 0..channel.height() {
         for pixel in channel.line_ref(line) {
-            if let Some(bin) = bins.get_mut((pixel - stats.min) as usize*size as usize/divisor) {
+            if let Some(bin) = bins.get_mut((pixel - stats.min) as usize * size as usize / divisor)
+            {
                 *bin += 1;
             }
         }
     }
 
-    Histogram { bins, min: stats.min, max: stats.max }
-}
-
-fn combine_stats(first: &ChannelStats, second: &ChannelStats) -> ChannelStats {
-    ChannelStats {
-        min: min(first.min, second.min),
-        max: max(first.max, second.max),
+    Histogram {
+        bins,
+        min: stats.min,
+        max: stats.max,
     }
 }
