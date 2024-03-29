4139d43465c3ebbd60458d699bd5991d544a0fce
Mit Bailey <mitbailey@outlook.com>
2024-03-29 17:44:56 -0400

    Expand tilde added

--- ccdi-logic/src/storage/save.rs
+++ ccdi-logic/src/storage/save.rs
@@ -1,7 +1,9 @@
 use std::path::PathBuf;
 
-use ccdi_common::{to_string, RawImage};
 use cameraunit::ImageData;
+use ccdi_common::{to_string, RawImage};
+
+use simple_expand_tilde::*;
 
 // ============================================ PUBLIC =============================================
 
@@ -9,10 +11,22 @@ pub fn save_fits_file(image: &RawImage, file_name: &str) -> Result<(), String> {
     let path = PathBuf::from(file_name);
     let prefix = path.parent().ok_or(format!("Invalid path parent"))?;
     println!("Prefix: {:?}", prefix);
-    std::fs::create_dir_all(prefix).map_err(to_string)?;
-
+    let prefix = if prefix.starts_with("~") {
+        expand_tilde(&prefix).ok_or(format!("Could not un-tilde"))?
+    } else {
+        prefix.to_owned()
+    };
+    println!("Prefix with tilde expansion: {:?}", prefix);
+    // let prefix = prefix
+    //     .canonicalize()
+    //     .map_err(|err| format!("Invalid path {:?} could not canonicalize", err))?;
+    // println!("After canonicalize: Prefix: {:?}", prefix);
+    std::fs::create_dir_all(&prefix).map_err(to_string)?;
+    println!(
+        "Saving to: {:?}", prefix
+    );
     let img = ImageData::from(image.data.clone());
-    img.save_fits(prefix, "ccdi", "CCDI ASI", true, true)
+    img.save_fits(&prefix, "ccdi", "CCDI ASI", true, true)
         .map_err(to_string)?;
 
     Ok(())
